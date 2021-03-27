6354035ccde0c441a27ff3a6568bed3969a6575c
Rachel <rachel.s.fearon@gmail.com>
2021-03-27 13:29:11 -0400

    iot inspiration board w sensehat display

--- IoT_inspiration_board/app.py
+++ IoT_inspiration_board/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, current_app as current_app
 from sense_hat import SenseHat
 
-sensor = SenseHat()
+sense = SenseHat()
 
 app = Flask(__name__)
 
@@ -10,10 +10,13 @@ def index():
     return render_template ("index.html")
 
 @app.route('/success', methods=['GET', 'POST'])
-def success(name):
+def success():
     message = request.form['message']
-    message = request.form['name']
-    return render_template("success.html", message = message, name = message)
+    name = request.form['name']
+    sense.show_message(message)
+    sense.show_message(name)
+    return render_template('success.html', message = message, name = name)
+    
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0') 
